dc2d4d55f868379ba7a5fd8505ad100d42148b07
Toluwaloju0 <teecoded1999@gmail.com>
2025-01-06 21:51:01 +0100

    routes

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import AppController from '../controllers/AppController';
-import UserController from '../controllers/UsersController';
-import AuthController from '../controllers/AuthController';
-import FilesController from '../controllers/FilesController';
+// import UserController from '../controllers/UsersController';
+// import AuthController from '../controllers/AuthController';
+// import FilesController from '../controllers/FilesController';
 
 const router = express.Router();
 router.use(express.json());
@@ -10,12 +10,12 @@ router.use(express.json());
 // Define each endpoints using the valid routes
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
-router.post('/users', UserController.postNew);
-router.get('/connect', AuthController.getConnect);
-router.get('/disconnect', AuthController.getDisconnect);
-router.get('/users/me', UserController.getMe);
-router.post('/files', FilesController.postUpload);
-router.get('/files/:id', FilesController.getShow);
+// router.post('/users', UserController.postNew);
+// router.get('/connect', AuthController.getConnect);
+// router.get('/disconnect', AuthController.getDisconnect);
+// router.get('/users/me', UserController.getMe);
+// router.post('/files', FilesController.postUpload);
+// router.get('/files/:id', FilesController.getShow);
 // router.get('files', FilesController.getIndex);
 
 export default router;
