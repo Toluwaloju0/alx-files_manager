33cf7fd85f59f6cb6af8c222082ba962f910ed00
Toluwaloju0 <teecoded1999@gmail.com>
2025-01-07 00:06:10 +0100

    routes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import AppController from '../controllers/AppController';
 import UserController from '../controllers/UsersController';
-import AuthController from '../controllers/AuthController';
+// import AuthController from '../controllers/AuthController';
 // import FilesController from '../controllers/FilesController';
 
 const router = express.Router();
@@ -11,9 +11,9 @@ router.use(express.json());
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 router.post('/users', UserController.postNew);
-router.get('/connect', AuthController.getConnect);
-router.get('/disconnect', AuthController.getDisconnect);
-router.get('/users/me', UserController.getMe);
+// router.get('/connect', AuthController.getConnect);
+// router.get('/disconnect', AuthController.getDisconnect);
+// router.get('/users/me', UserController.getMe);
 // router.post('/files', FilesController.postUpload);
 // router.get('/files/:id', FilesController.getShow);
 // router.get('files', FilesController.getIndex);
