007f4c63f157e1f7dec8887c47226fa75353c8ad
Toluwaloju0 <teecoded1999@gmail.com>
2025-01-09 07:47:36 +0100

    file

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import AppController from '../controllers/AppController';
 import UserController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
-import FilesController from '../controllers/FilesController';
+// import FilesController from '../controllers/FilesController';
 
 const router = express.Router();
 router.use(express.json());
@@ -14,8 +14,8 @@ router.post('/users', UserController.postNew);
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
 router.get('/users/me', UserController.getMe);
-router.post('/files', FilesController.postUpload);
-router.get('/files/:id', FilesController.getShow);
+// router.post('/files', FilesController.postUpload);
+// router.get('/files/:id', FilesController.getShow);
 // router.get('files', FilesController.getIndex);
 
 export default router;
