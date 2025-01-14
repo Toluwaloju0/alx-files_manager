2ff9d3e72034a6cf20157010fea0f341f6a47848
Toluwaloju0 <teecoded1999@gmail.com>
2025-01-14 20:15:56 +0100

    files manager

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -1,5 +1,5 @@
 import { ObjectId } from 'mongodb';
-import fs from 'fs';
+import fs from 'fs/promises';
 import { v4 } from 'uuid';
 import path from 'path';
 import decoder from '../utils/decoder';
@@ -13,32 +13,27 @@ const FilesController = {
     const userId = await redisClient.get(`auth_${token}`);
     const user = await dbClient.getData({ _id: ObjectId(userId) });
     if (user === null) {
-      res.status(401).json({ error: 'Unauthorized' }).end();
-      return { error: 'Unauthorized' };
+      return res.status(401).json({ error: 'Unauthorized' }).end();
     }
     // Declare the accpted types
     const typeVar = ['folder', 'file', 'image'];
     // Get the name and type from the request body
     const { name, type, data } = req.body;
     if (name === undefined) {
-      res.status(400).json({
+      return res.status(400).json({
         error: 'Missing name',
       }).end();
-      return { error: 'Missing name' };
     } if (type === undefined || !typeVar.includes(type)) {
-      res.status(400).json({
+      return res.status(400).json({
         error: 'Missing type',
       }).end();
-      return { error: 'Missing type' };
-    } if (type !== 'folder' && (data === undefined)) {
-      res.status(400).json({
+    } if (type !== 'folder' && data === undefined) {
+      return res.status(400).json({
         error: 'Missing data',
       }).end();
-      return { error: 'Missing data' };
     }
     // Define the parent id and the is public variables
-    let { parentId } = req.body;
-    let { isPublic } = req.body;
+    let { parentId, isPublic } = req.body;
     if (parentId === undefined) { parentId = '0'; }
     if (isPublic === undefined) { isPublic = false; }
     if (parentId !== '0') {
@@ -53,37 +48,27 @@ const FilesController = {
         }).end();
       }
     } else if (type === 'folder') {
-      const key = {
+      const id = await dbClient.saveFile({
         userId, name, type, parentId,
-      };
-      const id = await dbClient.saveFile(key);
+      });
       return res.status(201).json({
-        id, userId: ObjectId(userId), name, type, isPublic, parentId,
+        id, userId, name, type, isPublic, parentId,
       }).end();
     }
     // Get the folder path and the converted data given
     const folderPath = process.env.FOLDER_PATH || '/tmp/files_manager';
-    const clearData = decoder.dataDecoder(data);
     // check if the folder exists else create it
-    fs.stat(folderPath, (err, stat) => {
-      if (err || stat.isFile()) { fs.mkdirSync(folderPath); }
-    });
-    // create a path to store the data
+    await fs.mkdir(folderPath, { recursive: true })
+    // create a write stream and store the data in clear
     const filePath = `${folderPath}/${v4()}`;
-    const writeFile = fs.createWriteStream(filePath);
-    writeFile.write(clearData);
-    writeFile.end();
+    await fs.writeFile(filePath, decoder.dataDecoder(data), { mode: 0o666, flag: 'w' });
     const localPath = path.resolve(filePath);
-    const fileId = await dbClient.saveFile({
+    // Store the file in db
+    const id = await dbClient.saveFile({
       userId, name, type, isPublic, parentId, localPath,
     });
     return res.status(201).json({
-      id: fileId,
-      userId: ObjectId(userId),
-      name,
-      type,
-      isPublic,
-      parentId,
+      id, userId, name, type, isPublic, parentId,
     }).end();
   },
 
